d197357cb3ce32803880349444addcf02d2b560f
André Palóczy <paloczy@gmail.com>
2019-04-11 15:59:00 -0700

    Finish implementing the co-spectra fit method. Still needs testing

--- dewaveADCP/CospectraFit.py
+++ dewaveADCP/CospectraFit.py
@@ -58,7 +58,6 @@ def calc_Couwvw(Su1u1, Su2u2, Su3u3, Su4u4, theta):
     return Couw, Covw
 
 
-def calc_Sww(z, h, Spp):
 def calc_Sww(p, t, z, h, sw_disprel=False, rho=1024, fs=1.0, window='hanning', nperseg=1024, **kw):
     """
     USAGE
@@ -79,6 +78,165 @@ def calc_Sww(p, t, z, h, sw_disprel=False, rho=1024, fs=1.0, window='hanning', n
 
     # Convert pressure spectrum to w spectrum at each depth using linear wave theory.
     cff = (k/(rho*omega))**2
-    Sww = Spp*cff*np.tanh(k*(z + h))**2
+    tanhkh = np.tanh(k*(z[:,np.newaxis] + h))
+    Sww = Spp*cff*tanhkh**2
 
-    return Sww
+    omega = omega/(2*np.pi) # Back to linear frequency.
+
+    return omega, Sww
+
+
+def calc_uwvw(Couw, Covw, k0=1e-4):
+    """
+    USAGE
+    -----
+    uw, vw = calc_uwvw(Couw, Covw, k0=1e-4)
+
+    Integrate the uw, vw co-spectra from frequency zero up to the wave cutoff frequency
+    to obtain the de-waved Reynolds stresses <uw> and <vw>.
+    """
+    return uw, vw
+
+
+def get_kwc(cutoff_perc=0.3):
+    """
+    USAGE
+    -----
+    owc = get_kwc(cutoff_perc=0.3)
+
+    Calculate the wave cut-off wavenumber, defined
+    as the frequency where the Sww autospectrum
+    reaches 'cutoff_perc' percent of the along-beam
+    velocity autospectrum, Suiui.
+    """
+    raise NotImplementedError
+
+
+def Somega2Sk(omega, b1, b2, b3, b4, theta):
+    """
+    USAGE
+    -----
+    k = Somega2Sk(omega, b1, b2, b3, b4, theta)
+
+    Converts a frequency spectrum 'Sxx(omega)' to
+    a wavenumber spectrum 'Sxx(k)' by assuming Taylor's
+    or "Frozen Flow" Hypothesis.
+    """
+    # Use the burst-mean velocity profile.
+    b1 = np.nanmean(b1, axis=1)[:, np.newaxis]
+    b2 = np.nanmean(b2, axis=1)[:, np.newaxis]
+    b3 = np.nanmean(b3, axis=1)[:, np.newaxis]
+    b4 = np.nanmean(b4, axis=1)[:, np.newaxis]
+
+    Vx, Vy, _ = janus2xyz(b1, b2, b3, b4, theta)
+    U = np.sqrt(Vx**2 + Vy**2)
+    k = omega[np.newaxis,:]/U # omega = k*|U|.
+
+    return k
+
+
+def kamel72spec(k):
+    """Free parameters: c1, c2 = (uwstar, k0)."""
+    def fkamel72spec(k, c1, c2):
+        f73 = 7/3
+        C = f73*np.sin(np.pi/f73)/np.pi
+
+        return c1*C*(1/c2)/(1 + (k/c2)**f73)
+
+    return fkamel72spec
+
+
+def Couwvw_fit(k, Suiuik, func=kamel72spec, c1c2guess=(5e-4, 2)):
+    """..."""
+    c1c2, _ = curve_fit(func, k, Suiuik, p0=c1c2guess, maxfev=10000)
+    ustar, k0 = c1c2
+    uwvw_model = kamel72spec(k, ustar, k0)
+
+    return uwvw_model, ustar, k0
+
+
+def calcvp(v, normalize=True):
+    """
+    USAGE
+    -----
+    vp = calcvp(v, normalize=True)
+
+    Calculate turbulent velocity vp (v-primed) from full along-beam velocities.
+    "v" should have [nbeams x nbins x nens x nepochs] shape.
+
+    ***This is a direct translation of the Matlab function 'ADCP_SF_calc_vp.m'
+    by Brian Scannell, University of Bangor.
+
+    Translated by André Palóczy, Jan/2019.
+    apaloczy@ucsd.edu
+    % function vp = ADCP_SF_calc_vp(v,option)
+    %
+    % Function to calculate the turbulent velocity component v' from an array of
+    % velocity measurement produced by an HF ADCP operating in pulse-pulse
+    % coherent mode.  Calculated by deducting the mean background flow from the
+    % individual observations.
+    %
+    % Option to normalise the velocities by deducting the ensemble median from
+    % all bins in order to remove the static difference between ensembles prior
+    % to calculating the background shear.
+    %
+    % Inputs
+    %   v:  velocity array with dimensions [nbeams x nbins x nens x nepochs]
+    %       where nens is the number of ensembles (measurement profiles) in an
+    %       epoch (observational period).
+    %   option: flag to indicate whether to normalise the data prior to
+    %       removing the background shear to deduct. Allowed values:
+    %       option = 0, do NOT normalise
+    %       option = 1, normalise [default]
+    %
+    % BDS, Bangor January 2016
+    """
+    nz, nt = v.shape # [nbins x nens].
+
+    # deduct background shear (mean for each bin in each beam across all
+    # ensembles within each epoch) to give turbulent velocity variation
+    if normalize: # normalise data first if option is true
+        # deduct the median value across all the bins for each beam /
+        # ensemble / epoch to normalise each ensemble profile
+        vp = v - np.nanmedian(v, axis=0) # Remove z-average.
+
+        # now remove background shear
+        vp = vp - np.nanmean(vp, axis=1)[..., np.newaxis]
+    else: # remove backbround shear without normalising data first
+        vp = v - np.nanmean(v[np.newaxis, ...], axis=1) # Remove t-average.
+
+    return vp
+
+
+def ksgw(omega, h):
+	"""
+	The code below was translated to Python by André Palóczy
+	from the original Matlab code "ib09_get_wavenumber.m"
+	by Falk Feddersen.
+
+	# % Falk Feddersen (c) 2001
+	# %
+	# % function that takes the radian wave frequency and
+	# % a vector of depths and returns the wavenumber at that
+	# % depth by solving the dispersion relationship
+	# %
+	# % function k = get_wavenum(omega,h)
+
+	# function k = ib09_get_wavenumber(omega,h)
+
+	# % returns the wavenumber of the gravity wave
+	# % dispersion relation, by using newtons method
+
+	# % the initial guess will be the shallow water wavenumber
+	"""
+	g = 9.81 # [m/s2].
+
+	k = omega/np.sqrt(g*h) # Shallow water wavenumber.
+	f = g*k*np.tanh(k*h) - omega**2
+
+	while np.max(np.abs(f))>1e-10:
+		dfdk = g*k*h*(1./np.cosh(k*h))**2 + g*np.tanh(k*h)
+		k = k - f/dfdk
+		f = g*k*np.tanh(k*h) - omega**2
+
+	return k
