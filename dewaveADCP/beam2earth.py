58a8facab8be808f8fce65c1862a63e2150a8799
André Palóczy <paloczy@gmail.com>
2019-10-11 17:41:32 -0700

    Minor translation syntax fixes, implement janus2earth5().

--- dewaveADCP/beam2earth.py
+++ dewaveADCP/beam2earth.py
@@ -7,11 +7,13 @@ from .utils import sind, cosd
 ######################
 #### 4-beam Janus ####
 ######################
-def janus2xyz(b1, b2, b3, b4, theta, r=None, binmaptype='linear', ptch=None, roll=None, use3beamsol=True, verbose=True):
+def janus2xyz(b1, b2, b3, b4, theta, r=None, binmaptype=None, ptch=None, roll=None, use3beamsol=True, verbose=True):
     """
     USAGE
     -----
-    vx, vy, vz = janus2xyz(b1, b2, b3, b4, theta, r=None, binmaptype='linear', ptch=None, roll=None, use3beamsol=True, verbose=True)
+    vx, vy, vz = janus2xyz(b1, b2, b3, b4, theta, r=None, binmaptype=None, ptch=None, roll=None, use3beamsol=True, verbose=True)
+
+    theta, ptch, roll must be in RADIANS.
     """
     Nz, Nt = b1.shape
     if binmaptype is not None:
@@ -52,11 +54,11 @@ def janus2xyz(b1, b2, b3, b4, theta, r=None, binmaptype='linear', ptch=None, rol
     return Vx, Vy, Vz
 
 
-def janus2earth(head, ptch, roll, theta, b1, b2, b3, b4, r=None, gimbaled=True, binmaptype='linear', use3beamsol=True, verbose=True):
+def janus2earth(head, ptch, roll, theta, b1, b2, b3, b4, r=None, gimbaled=True, binmaptype=None, use3beamsol=True, verbose=True):
     """
      USAGE
      -----
-     [u, v, w] = janus2earth(head, ptch, roll, theta, b1, b2, b3, b4, r=None, gimbaled=True, binmaptype='linear', use3beamsol=True, verbose=True)
+     [u, v, w] = janus2earth(head, ptch, roll, theta, b1, b2, b3, b4, r=None, gimbaled=True, binmaptype=None, use3beamsol=True, verbose=True)
 
      Calculates Earth velocities (u,v,w) = (east,north,up) from beam-referenced velocity time series
      from a 4-beam Janus ADCP, (e.g., Appendix A of Dewey & Stringer (2007), Equations A3-A11).
@@ -129,7 +131,7 @@ def janus2earth(head, ptch, roll, theta, b1, b2, b3, b4, r=None, gimbaled=True, 
                                      heading angle if the ADCP was mounted on a gimbal (Dewey &
                                      Stringer, 2007; Lohrmann et al., 1990).
 
-     binmaptype ['none' or 'linear' or 'nn']
+     binmaptype [None or 'linear' or 'nn']
                                       Whether to map the beam velocities to fixed horizontal
                                       planes with linear interpolation ('linear') or nearest-neighbor
                                       interpolation ('nearest') prior to converting
@@ -141,12 +143,11 @@ def janus2earth(head, ptch, roll, theta, b1, b2, b3, b4, r=None, gimbaled=True, 
 
      OUTPUTS
      -------
-     [u, v, w]           [east, north, up, up-(from vertical beam only)] components
-                          of Earth-referenced velocity vector.
+     [u, v, w]           [east, north, up] components of Earth-referenced velocity vector.
     """
     nz, nt = b1.shape # Number of vertical bins and records in the time series.
 
-    d2r = pi/180
+    d2r = np.pi/180
     head = head*d2r
     ptch = ptch*d2r
     roll = roll*d2r
@@ -163,12 +164,12 @@ def janus2earth(head, ptch, roll, theta, b1, b2, b3, b4, r=None, gimbaled=True, 
     if gimbaled: # Correct heading (D&S 2007, eq. A2).
         print('Gimbaled instrument case.')
         Sph2Sph3 = Sph2*Sph3
-        head = head + np.asin( Sph2Sph3/np.sqrt(Cph2**2 + Sph2Sph3**2) )
+        head = head + np.arcsin( Sph2Sph3/np.sqrt(Cph2**2 + Sph2Sph3**2) )
         Sph1 = np.sin(head)
         Cph1 = np.cos(head)
     else:                      # Correct pitch (D&S 2007, eq. A1 Lohrmann et al. 1990, eq. A1).
         print('Fixed instrument case.')
-        ptch = np.asin( (Sph2*Cph3)/np.sqrt(1 - (Sph2*Sph3)**2) )
+        ptch = np.arcsin( (Sph2*Cph3)/np.sqrt(1 - (Sph2*Sph3)**2) )
         Sph2 = np.sin(ptch)
         Cph2 = np.cos(ptch)
 
@@ -202,11 +203,13 @@ def janus2earth(head, ptch, roll, theta, b1, b2, b3, b4, r=None, gimbaled=True, 
 ######################
 #### 5-beam Janus ####
 ######################
-def janus2xyz5(b1, b2, b3, b4, b5, theta, binmaptype='linear', use3beamsol=True, verbose=True):
+def janus2xyz5(b1, b2, b3, b4, b5, theta, r=None, ptch=None, roll=None, binmaptype=None, use3beamsol=True, verbose=True):
     """
     USAGE
     -----
-    vx, vy, vz = janus2xyz5(b1, b2, b3, b4, b5, theta, binmaptype='linear', use3beamsol=True, verbose=True)
+    vx, vy, vz = janus2xyz5(b1, b2, b3, b4, b5, theta, r=None, ptch=ptch, roll=roll, binmaptype=None, use3beamsol=True, verbose=True)
+
+    theta, ptch, roll must be in RADIANS.
     """
     Nz, Nt = b1.shape
     if binmaptype is not None:
@@ -250,8 +253,166 @@ def janus2xyz5(b1, b2, b3, b4, b5, theta, binmaptype='linear', use3beamsol=True,
     return Vx, Vy, Vz, Vz5
 
 
-def janus2earth5():
-    raise NotImplementedError
+def janus2earth5(head, ptch, roll, theta, b1, b2, b3, b4, b5, r=None, gimbaled=True, binmaptype=None, uvwbeam5=True, use3beamsol=True, verbose=True):
+    """
+     USAGE
+     -----
+     u, v, w, w5 = janus2earth5(head, ptch, roll, theta, b1, b2, b3, b4, b5, r=None, gimbaled=True, binmaptype=None, uvwbeam5=True, use3beamsol=True, verbose=True)
+
+     Calculates Earth velocities (u,v,w) = (east,north,up) from beam-referenced velocity time series
+     from a 5-beam Janus ADCP, (e.g., Appendix A of Dewey & Stringer (2007), Equations A3-A11).
+
+     nz, nt, nb = number of vertical bins, data records, beams.
+
+    ============================================================================
+     For TRDI instruments, call function like this:
+     u, v, w = janus2earth(head, ptch, roll, theta, b1, b2, b3, b4)
+
+     For Nortek instruments, call function like this:
+     u, v, w = janus2earth(head-90, roll, -ptch, theta, -b1, -b3, -b4, -b2)
+    ============================================================================
+
+        TRDI CONVENTION:
+        ================
+
+     * Velocity toward transducers' faces: POSITIVE
+     * Clockwise PITCH (tilt about x-AXIS): POSITIVE (beam 3 higher than beam 4)
+     * Clockwise ROLL (tilt about y-AXIS):  POSITIVE (beam 2 higher than beam 1)
+
+     * Heading increases COUNTER-CLOCKWISE from the *Y-AXIS*.
+
+           ^ positive y axis, psi = 0
+           |
+           3
+           |
+           |
+           |
+     2 --- O --- 1 ---> positive x axis, psi = -90
+           |
+           |
+           |
+           4
+
+        NORTEK CONVENTION:
+        ==================
+
+     * Velocity toward transducers' faces: NEGATIVE
+     * Counter-clockwise PITCH (tilt about y-AXIS, equivalent to -ROLL in the TRDI convention): POSITIVE (beam 1 higher than beam 3)
+     * Clockwise ROLL (tilt about x-AXIS, equivalent to PITCH in the TRDI convention):  POSITIVE (beam 4 higher than beam 2)
+
+     Heading increases CLOCKWISE from the *X-AXIS*.
+
+           ^ positive y axis, psi = -90
+           |
+           4
+           |
+           |
+           |
+     3 --- O --- 1 ---> positive x axis, psi = 0
+           |
+           |
+           |
+           2
+
+     INPUTS
+     ------
+     b1, b2, b3, b4, b5    [nz -by- nt] matrices of along-beam velocity components.
+     head, ptch, roll      [nt]         vectors with (time-dependent) heading, pitch
+                                        and roll angles, following D&S2007's notation.
+
+     theta                              Beam angle measured from the vertical.
+                                        *For RDI Sentinel V and Nortek Signature: 25.
+
+     uvwBeam5     [True or False]    whether to calculate [u, v, w] using the independent information
+                                     from beam 5 (defaults true). If false, the usual four-beam
+                                     solution using w derived from beams 1-4 is calculated.
+
+     gimbaled     [True or False]    Whether the ADCP was deployed with a gimbaled roll sensor
+                                     (default true). Applies the correction to the raw pitch angle
+                                     if the pitch/roll sensors were mounted rigidly to the
+                                     instrument ('Gimbaled'==false), or the correction to the raw
+                                     heading angle if the ADCP was mounted on a gimbal (Dewey &
+                                     Stringer, 2007; Lohrmann et al., 1990).
+
+     binmaptype [None or 'linear' or 'nn']
+                                      Whether to map the beam velocities to fixed horizontal
+                                      planes with linear interpolation ('linear') or nearest-neighbor
+                                      interpolation ('nearest') prior to converting
+                                      to instrument coordinates (Ott, 2002; Dewey & Stringer, 2007).
+                                      *The default is to NOT perform any bin mapping.
+
+     use3beamsol [True or False]      Whether to use three-beam solutions when exactly one beam has
+                                      no data in one cell.
+
+     OUTPUTS
+     -------
+     [u, v, w, w5]           [east, north, up, up-(from vertical beam only)] components
+                              of Earth-referenced velocity vector.
+    """
+    nz, nt = b1.shape # Number of vertical bins and records in the time series.
+
+    d2r = np.pi/180
+    head = head*d2r
+    ptch = ptch*d2r
+    roll = roll*d2r
+    theta = theta*d2r
+
+    # Time-dependent angles (heading, pitch and roll).
+    Sph1 = np.sin(head)
+    Sph2 = np.sin(ptch)
+    Sph3 = np.sin(roll)
+    Cph1 = np.cos(head)
+    Cph2 = np.cos(ptch)
+    Cph3 = np.cos(roll)
+
+    if gimbaled: # Correct heading (D&S 2007, eq. A2).
+        print('Gimbaled instrument case.')
+        Sph2Sph3 = Sph2*Sph3
+        head = head + np.arcsin( Sph2Sph3/np.sqrt(Cph2**2 + Sph2Sph3**2) )
+        Sph1 = np.sin(head)
+        Cph1 = np.cos(head)
+    else:                      # Correct pitch (D&S 2007, eq. A1 Lohrmann et al. 1990, eq. A1).
+        print('Fixed instrument case.')
+        ptch = np.arcsin( (Sph2*Cph3)/np.sqrt(1 - (Sph2*Sph3)**2) )
+        Sph2 = np.sin(ptch)
+        Cph2 = np.cos(ptch)
+
+    # Convert instrument-referenced velocities
+    # to Earth-referenced velocities.
+    # Option 1: Classic four-beam solution.
+    # Option 2: five-beam solution for [u, v, w].
+    cx1 = Cph1*Cph3 + Sph1*Sph2*Sph3
+    cx2 = Sph1*Cph3 - Cph1*Sph2*Sph3
+    cx3 = Cph2*Sph3
+    cy1 = Sph1*Cph2
+    cy2 = Cph1*Cph2
+    cy3 = Sph2
+    cz1 = Cph1*Sph3 - Sph1*Sph2*Cph3
+    cz2 = Sph1*Sph3 + Cph1*Sph2*Cph3
+    cz3 = Cph2*Cph3
+
+    # Convert beam-referenced velocities to instrument-referenced velocities.
+    # NOTE: The convention used here (positive x axis = horizontally away from beam 1) and
+    #                                 positive y axis = horizontally away from beam 3) is not
+    #                                 the same as the one used by the instrument's firmware if
+    #                                 the coordinate transformation mode is set to "instrument
+    #                                 coordinates" before deployment.
+    Vx, Vy, Vz, Vz5 = janus2xyz5(b1, b2, b3, b4, b5, theta, r=r, ptch=ptch, roll=roll, binmaptype=binmaptype, use3beamsol=use3beamsol, verbose=verbose)
+
+    w5 = Vz5*cz3 # w from beam 5 only.
+
+    if uvwbeam5:
+        print('Using vertical beam for [u, v, w].')
+        u = +Vx*cx1 + Vy*cy1 + Vz5*cz1
+        v = -Vx*cx2 + Vy*cy2 - Vz5*cz2
+        w = -Vx*cx3 + Vy*cy3 + w5
+    else:
+        print('Using only beams 1-4 for [u, v, w].')
+        u = +Vx*cx1 + Vy*cy1 + Vz*cz1
+        v = -Vx*cx2 + Vy*cy2 - Vz*cz2
+        w = -Vx*cx3 + Vy*cy3 + Vz*cz3
+
+    return u, v, w, w5
 
 
 def binmap5(b1, b2, b3, b4, b5, r, r5, theta, ptch, roll, how='linear'):
