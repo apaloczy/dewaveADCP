9b581058a59222343096ac1a3f1458c317dc74ab
André Palóczy <paloczy@gmail.com>
2020-04-16 10:57:36 -0700

    Adds 'bvelAF()' function

--- dewaveADCP/AdaptiveFiltering.py
+++ dewaveADCP/AdaptiveFiltering.py
@@ -6,7 +6,9 @@ def bvarAF(b, sep=6, Lw=8, max_badfrac=0.5, mindet=1e-4, verbose=False):
     """
     USAGE
     -----
-    bvar_dewaved = bvarAF(bvel, sep=6, Lw=8, max_badfrac=0.5, verbose=False)
+    bvar_dewaved = bvarAF(bvel, sep=6, Lw=8, mindet=1e-4, max_badfrac=0.5, verbose=False)
+
+    Returns de-waved, time-averaged beam variances.
     """
     nz, nt = b.shape
     bvardw = np.empty(nz)*np.nan
@@ -60,6 +62,73 @@ def bvarAF(b, sep=6, Lw=8, max_badfrac=0.5, mindet=1e-4, verbose=False):
         bvardw[k] = np.mean((ub1d - ub1d.mean())**2)
 
     return bvardw
+
+
+def bvelAF(b, sep=6, Lw=8, max_badfrac=0.5, mindet=1e-4, verbose=False):
+    """
+    USAGE
+    -----
+    bvvel_dewaved = bvelAF(bvel, sep=6, Lw=8, mindet=1e-4, max_badfrac=0.5, verbose=False)
+
+    Returns de-waved beam velocities (not time-averaged).
+    """
+    nz, nt = b.shape
+    bveldw = np.empty((nz, nt))*np.nan
+    b = b.T # Each row is a timestamp.
+    for k in range(nz-sep):
+        ub1 = b[:,k]
+        ub2 = b[:,k+sep]
+        # Take only timestamps where both levels are non-NaN.
+        fub1ub2 = np.logical_and(np.isfinite(ub1), np.isfinite(ub2))
+        ifub1ub2 = np.where(fub1ub2)[0]
+        percg = fub1ub2.sum()/nt
+        if percg<(1-max_badfrac):
+            if verbose:
+                print("More than ", 100*max_badfrac,"%% NaNs, skipping bin #%d."%k)
+            continue
+        if verbose:
+            print("Using only non-NaNs (%.1f%% data points, bin #%d)"%(100*percg,k+1))
+
+        ub1 = ub1[fub1ub2]
+        ub2 = ub2[fub1ub2]
+
+        # Remove time-means.
+        ub1 = ub1 - np.nanmean(ub1)
+        ub2 = ub2 - np.nanmean(ub2)
+
+        # Make windowed data matrix A from velocities at bin 2 (upper).
+        ntt = ub2.size
+        A = np.matrix(np.empty((ntt-Lw, Lw)))*np.nan
+        for i in range(0, ntt-Lw):
+            A[i,:] = ub2[i:i+Lw]
+
+        # Calculate the weights estimator ŝ = (A.T*A).I*A.T*ub1 and
+        # use ŝ to calculate ub1h, the predicted velocities at the
+        # *lower* bin, based on velocities at the *upper* bin.
+        AT = A.T
+        ATA = AT*A
+        detATA = np.abs(np.linalg.det(ATA))
+        if detATA<mindet:
+            if verbose:
+                print("AT*A determinant too low (%e), skipping bin #%d."%(detATA, k))
+            continue
+        else:
+            try:
+                s = np.linalg.solve(ATA, AT*np.matrix(ub1[:-Lw]).T)
+            except np.linalg.LinAlgError:
+                if verbose:
+                    print("Singular matrix, skipping bin #%d."%(k+1))
+                continue
+        ub1h = np.array(A*s).squeeze() # Vertically coherent velocity, assumed to be due to waves only.
+        ub1d = ub1[:-Lw] - ub1h        # De-waved velocity.
+
+        ub1d = ub1d.tolist()
+        ub1d.reverse()
+        for i in ifub1ub2:
+            if len(ub1d)>0:
+                bveldw[k, i] = ub1d.pop()
+
+    return bveldw
 
 
 def interpub1ub2(ub1, ub2, t, max_badfrac, k, verbose=False):
